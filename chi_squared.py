98ed780648e24a8f4859387d80db6878518ef339
IvanLeung36 <ivanleung36@gmail.com>
2024-09-30 00:43:05 +0100

    Changed chi_square into a function

--- chi_squared.py
+++ chi_squared.py
@@ -1,18 +1,19 @@
-data = []
-expected = []
-chi_square = 0
+def chi_square(data, expected):
+    # data = []
+    # expected = []
+    chi_square = 0
 
-length = int(input("How long is the data set? "))
-for i in range(length):
-    u_input = float(input(f"Number {i+1}: "))
-    data.append(u_input)
-print("EXPECTED VALUE")
-for i in range(length):
-    u_input = float(input(f"Number {i+1}: "))
-    expected.append(u_input)
+    # length = int(input("How long is the data set? "))
+    # for i in range(length):
+    #     u_input = float(input(f"Number {i+1}: "))
+    #     data.append(u_input)
+    # print("EXPECTED VALUE")
+    # for i in range(length):
+    #     u_input = float(input(f"Number {i+1}: "))
+    #     expected.append(u_input)
 
-for i in range(length   ):
-    if expected[i] != 0:
-        chi_square += ((data[i]-expected[i])**2)/expected[i]
+    for i in range(len(data)):
+        if expected[i] != 0:
+            chi_square += ((data[i]-expected[i])**2)/expected[i]
 
-print(chi_square)
+    return(chi_square)
